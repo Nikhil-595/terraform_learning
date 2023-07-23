622e254413887ecfd38d6a174bba86f2abfa3834
nalinture <68896641+nalinture@users.noreply.github.com>
2023-07-23 18:31:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ variable "github_token" {
     default = "cooku_with_comali-04"
 }
 
-resource "github_repository" "iac-github" {
-  name        = "iac-github"
+resource "github_repository" "iac-github-01" {
+  name        = "iac-github-01"
   description = "this github repo was created and managed using terraform"
   auto_init = true
   #private = false
@@ -29,27 +29,27 @@ resource "github_repository" "iac-github" {
 }
 
 resource "github_branch" "dev" {
-  repository = "iac-github"
+  repository = "iac-github-01"
   branch     = "dev"
   source_branch = "master"
 
   depends_on = [
-    github_repository.iac-github
+    github_repository.iac-github-01
   ]
 }
 
-resource "github_repository_file" "iac-github" {
-repository          = github_repository.iac-github.name
+resource "github_repository_file" "iac-github-01" {
+repository          = github_repository.iac-github-01.name
 branch              = "master"
 file                = ".gitignore"
 content             = "**/*.tfstate"
-commit_message      = "Managed by Terraform"
+commit_message      = "Managed by github.com/nalinture/terraform_learning"
 commit_author       = "Terraform User"
 commit_email        = "nalinkumarmurugesan@gmail.com"
 overwrite_on_create = true
 
   depends_on = [
-    github_repository.iac-github
+    github_repository.iac-github-01
   ]
 
 
