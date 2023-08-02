c14506f52e7d3434b66bdf9b5f51f117bc066d26
nalinture <68896641+nalinture@users.noreply.github.com>
2023-08-02 17:48:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ variable "github_token" {
     default = "cooku_with_comali-04"
 }
 
-resource "github_repository" "iac-github-12" {
-  name        = "iac-github-12"
+resource "github_repository" "repo" {
+  name        = "iac-github-13"
   description = "this github repo was created and managed using terraform"
   auto_init = true
   #private = false
@@ -29,17 +29,17 @@ resource "github_repository" "iac-github-12" {
 }
 
 resource "github_branch" "dev" {
-  repository = "iac-github-12"
+  repository = "iac-github-13"
   branch     = "dev"
   source_branch = "master"
 
   depends_on = [
-    github_repository.iac-github-12
+    github_repository.repo
   ]
 }
 
-resource "github_repository_file" "iac-github-12" {
-repository          = github_repository.iac-github-12.name
+resource "github_repository_file" "file" {
+repository          = github_repository.repo.name
 branch              = "master"
 file                = ".gitignore"
 content             = "**/*.tfstate"
@@ -49,7 +49,7 @@ commit_email        = "nalinkumarmurugesan@gmail.com"
 overwrite_on_create = true
 
   depends_on = [
-    github_repository.iac-github-12
+    github_repository.repo
   ]
 
 
